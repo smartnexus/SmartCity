97cb1179dc5bc483b4829596f3a3c8c27e428dfc
smartnexus <angrodboh@alum.us.es>
2021-04-17 19:13:32 +0200

    Timer implementado

--- src/dit/sdsw/sensors/SensorParking.java
+++ src/dit/sdsw/sensors/SensorParking.java
@@ -1,19 +1,45 @@
 package dit.sdsw.sensors;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SensorParking {
 	
-	int plazas;
+	private int plazas; //Plazas ocupadas.
+	private int limite;
 	
 	public SensorParking() {
 		plazas = 0;
-	}
-	
-	public int getPlazas() {
-		return plazas;
+		limite = 20;
+		
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {  
+            @Override
+            public void run() {  
+            	Random random = new Random();
+            	int diff = (random.nextInt(21) - 10);
+            	
+            	if(diff < 0 && (plazas + diff) < 0)
+            		diff = plazas;
+            	else if(diff > 0 && (plazas + diff) > limite)	
+            		diff = limite - plazas;
+            	
+            	plazas += diff;
+           }
+		}, 0, 1000);
 	}
 
 	public void setPlazas(int plazas) {
 		this.plazas = plazas;
+	}
+	
+	public static void main(String[] args) throws InterruptedException {
+		SensorParking sensor = new SensorParking();
+		Thread.sleep(10000);
+		System.out.println("Running:" + sensor.plazas);
+		Thread.sleep(10000);
+		System.out.println("Running:" + sensor.plazas);
 	}
 
 }
