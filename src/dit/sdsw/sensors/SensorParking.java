5adbb95ce35c832c3acd5a825b5d5f80feed2dcc
smartnexus <angrodboh@alum.us.es>
2021-04-17 18:03:12 +0200

    Creando esqueleto de sensor

--- src/dit/sdsw/sensors/SensorParking.java
+++ src/dit/sdsw/sensors/SensorParking.java
@@ -1,5 +1,19 @@
 package dit.sdsw.sensors;
 
 public class SensorParking {
+	
+	int plazas;
+	
+	public SensorParking() {
+		plazas = 0;
+	}
+	
+	public int getPlazas() {
+		return plazas;
+	}
+
+	public void setPlazas(int plazas) {
+		this.plazas = plazas;
+	}
 
 }
