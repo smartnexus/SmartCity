8ae6c234f1d3e8fc736d8574938f8a241e18e89d
celllarod <celllarod@alum.us.es>
2021-04-17 19:47:57 +0200

    SensorContenedor añadido

--- src/dit/sdsw/sensors/SensorContenedor.java
+++ src/dit/sdsw/sensors/SensorContenedor.java
@@ -1,5 +1,54 @@
 package dit.sdsw.sensors;
 
-public class SensorContenedor {
+import java.util.Timer;
+import java.util.TimerTask;
+import dit.sdsw.Utils;
 
+public class SensorContenedor {
+	private int nivel;       //Medido en centímetros
+	private int nivelMax; 
+   
+	public SensorContenedor(int nivelMax) {
+		this.nivel = 0;
+		this.nivelMax = nivelMax;
+		
+		//Simulamos el incremento del nivel de basura en el contenedor de forma aleatoria.
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {  
+				@Override
+	            public void run() {  
+	            	setNivel(Utils.randomIncrement(0, 20, nivel, nivelMax));
+	           }
+			}, 0, 1000);
+		
+	}
+
+	public int getNivelMax() {
+		return nivelMax;
+	}
+
+	public void setNivelMax(int nivelMax) {
+		this.nivelMax = nivelMax;
+	}
+
+	public int getNivel() {
+		return nivel;
+	}
+	
+	public void setNivel(int nivel) {
+		this.nivel = nivel;
+	}	
+	/*	public static void main(String args[]) throws InterruptedException {
+		SensorContenedor sensor = new SensorContenedor(150);
+		while(true) {
+			Thread.sleep(1*1000);
+			System.out.println(sensor.getNivel());
+			
+			if(sensor.getNivel() == 150) {
+				sensor.setNivel(0);
+			}
+		}		
+	}
+	 */
 }
+
