9d69e7981d728ee668ef24c7245c90ce4ac6ef4a
Angel Rodriguez <angrodboh@alum.us.es>
2021-04-17 19:24:44 +0200

    Con mínimo 0 no hace falta onlyIncrement

--- src/dit/sdsw/Utils.java
+++ src/dit/sdsw/Utils.java
@@ -9,15 +9,14 @@ public class Utils {
 	 * @param max -> Maximum value to inc/dec
 	 * @param value -> Current value
 	 * @param limit -> Maximum value operation can reach
-	 * @param onlyIncrement -> True if operation should only increment
 	 * @return new value
 	 */
-	public static Integer randomIncrement(int min, int max, int value, int limit, boolean onlyIncrement)  {
+	public static Integer randomIncrement(int min, int max, int value, int limit)  {
 		Random random = new Random();
     	int diff = (random.nextInt((max - min) + 1) + min);
     	
     	if(diff < 0 && (value + diff) < 0)
-    		diff = onlyIncrement ? 0 : value;
+    		diff = value;
     	else if(diff > 0 && (value + diff) > limit)	
     		diff = limit - value;
     	
