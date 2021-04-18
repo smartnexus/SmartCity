104cc020ade0bf6e0e1d7a9436d280c09d01ae6c
smartnexus <angrodboh@alum.us.es>
2021-04-18 13:05:03 +0200

    Implementación servicio terminada

--- src/dit/sdsw/server/services/ServicioParkingImpl.java
+++ src/dit/sdsw/server/services/ServicioParkingImpl.java
@@ -1,5 +1,43 @@
 package dit.sdsw.server.services;
 
-public class ServicioParkingImpl {
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class ServicioParkingImpl extends UnicastRemoteObject implements ServicioParking {
+
+	private static final long serialVersionUID = 2429902483238591715L;
+	
+	private String nombre;
+	private float latitud;
+	private float longitud;
+	private int capacidadTotal;
+	private boolean abierto;
+	private int plazasOcupadas;
+
+	public ServicioParkingImpl(String nombre, float latitud, float longitud, int capacidadTotal, 
+			boolean abierto, int plazasOcupadas) throws RemoteException {
+		
+		this.nombre = nombre;
+		this.latitud = latitud;
+		this.longitud = longitud;
+		this.capacidadTotal = capacidadTotal;
+		this.abierto = abierto;
+		this.plazasOcupadas = plazasOcupadas;
+	}
+
+	public void alertarAbierto() throws RemoteException {
+		this.abierto = true;
+		
+	}
+
+	public void alertarCerrado() throws RemoteException {
+		this.abierto = false;
+		
+	}
+
+	public void cambiarPlazasOcupadas(int plazasOcupadas) throws RemoteException {
+		this.plazasOcupadas = plazasOcupadas;
+		
+	}
 
 }
